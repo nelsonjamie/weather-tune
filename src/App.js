e9a4eefae74354ece9edc20ae1045bd72dca5658
KararAlsh <kararabdulsahib@hotmail.com>
2022-10-03 20:22:20 +0700

    added spotify authentification and some hardcoded html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios'
 import './App.css';
 
+
 function App() {
+	//for Spotify Api
+	const CLIENT_ID = '874bc4727f4046dfb6ace3b47584305f'
+	const REDIRECT_URI = 'http://localhost:3000/'
+	const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+	const RESPONSE_TYPE = 'token'
+
+	// for Weather Api
+	// const url = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=a8bb6300ed41f0f14aca082acab36629'
+
+
   return (
+
+
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<div className="container">
+				<div className="top">
+					<div className="location">
+						<p>Stockholm</p>
+					</div>
+					<div className="temp">
+						<h1>40℃</h1>
+					</div>
+					<div className="description">
+						<p>Clouds</p>
+					</div>
+				</div>
+				<div className="bottom">
+					<div className="feels">
+						<h1>30℃</h1>
+					</div>
+					<div className="humidity">
+						<h1>10%</h1>
+					</div>
+					<div className="wind">
+						<h1>5 mph</h1>
+					</div>
+					<a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Connect to Spotify</a>
+				</div>
+			</div>
+
     </div>
   );
 }
