47d4d74ecdd62c275df3c9da66ccf46d431a08cf
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-10-04 12:09:27 +0700

    Updated App structure, added location code and weather templates

--- src/App.js
+++ src/App.js
@@ -3,52 +3,88 @@ import axios from 'axios'
 import './App.css';
 
 
-function App() {
-	//for Spotify Api
-	const CLIENT_ID = '874bc4727f4046dfb6ace3b47584305f'
-	const REDIRECT_URI = 'http://localhost:3000/'
-	const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
-	const RESPONSE_TYPE = 'token'
+class App extends React.Component {
+	state = {
+		userLoc: 'Tucson',
+		weather: 'sunny',
+		longitude: '0',
+		latitude: '0',
+		LocKey: '438540'
+	}
+
+	// //for Spotify Api
+	// const CLIENT_ID = '874bc4727f4046dfb6ace3b47584305f'
+	// const REDIRECT_URI = 'http://localhost:3000/'
+	// const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+	// const RESPONSE_TYPE = 'token'
 
 	// for Weather Api
 	// const url = 'https://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=a8bb6300ed41f0f14aca082acab36629'
 
+	componentDidMount() {
+	    if ("geolocation" in navigator) {
+	      console.log("Available")
+				navigator.geolocation.getCurrentPosition((position) => {
+					console.log("Latitude is :", position.coords.latitude)
+		      console.log("Longitude is :", position.coords.longitude)
+					this.setState({
+						latitude: position.coords.latitude,
+						longitude: position.coords.longitude
+					})
+					console.log(this.state.latitude)
+	    	})
+	    } else {
+	      console.log("Not Available");
+	    }
+	  }
 
-  return (
+	// 	getLocKey = async (latitude, longitude) => {
+	// 		let LocKey = await axios.get('http://dataservice.accuweather.com/locations/v1/cities/geoposition/search')
+	// 		console.log(LocKey)
+	// 	}
+	//
+	// getWeather = async () => {
+	// 	let weather = await axios.get('http://dataservice.accuweather.com/currentconditions/v1/')
+	// 	console.log(weather)
+	// 	return weather
+	// }
 
 
-
-    <div className="app">
-			<div className="container">
-				<div className="top">
-					<div className="location">
-						<p>Stockholm</p>
+	render() {
+		return (
+			<div className="app">
+				<div className="container">
+					<div className="top">
+						<div className="location">
+							<p>Stockholm</p>
+						</div>
+						<div className="temp">
+							<h1>40℃</h1>
+						</div>
+						<div className="description">
+							<p>Clouds</p>
+						</div>
 					</div>
-					<div className="temp">
-						<h1>40℃</h1>
+					<div className="bottom">
+						<div className="feels">
+							<p className='bold'>30℃</p>
+							<p>Feels like</p>
+						</div>
+						<div className="humidity">
+							<p className='bold'>10%</p>
+							<p>Humidity</p>
+						</div>
+						<div className="wind">
+							<p>5 MPH</p>
+							<p>Wind Speed</p>
+						</div>
 					</div>
-					<div className="description">
-						<p>Clouds</p>
-					</div>
-				</div>
-				<div className="bottom">
-					<div className="feels">
-						<p className='bold'>30℃</p>
-						<p>Feels like</p>
-					</div>
-					<div className="humidity">
-						<p className='bold'>10%</p>
-						<p>Humidity</p>
-					</div>
-					<div className="wind">
-						<p>5 MPH</p>
-						<p>Wind Speed</p>
-					</div>
-					<a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Connect to Spotify</a>
 				</div>
 			</div>
-    </div>
-  );
+
+	)
+	}
+
 }
 
 export default App;
