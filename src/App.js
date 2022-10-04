328e5e234c3c21de25f246891a356128841e0629
KararAlsh <kararabdulsahib@hotmail.com>
2022-10-04 14:40:03 +0700

    console log for the spotify api

--- src/App.js
+++ src/App.js
@@ -80,7 +80,6 @@ class App extends React.Component {
 					</div>
 				</div>
 			</div>
-
 	)
 	}
 }
--- src/spotify.js
+++ src/spotify.js
@@ -5,3 +5,5 @@ const CLIENT_ID = '874bc4727f4046dfb6ace3b47584305f'
 	const RESPONSE_TYPE = 'token'
 
 export const loginEndpoint = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
+
+console.log({ loginEndpoint });
