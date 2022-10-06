e7c75a6c674ad2e660ab5ba86177c24fae585962
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-10-06 18:08:31 +0700

    Moved getLocation back to App.js

--- src/weatherFunctions.js
+++ src/weatherFunctions.js
@@ -1,23 +1,25 @@
 import axios from 'axios'
 
+//Gets the current weather (pass results of getlocation)
+const getWeather = async (a, b) => {
+	console.log("getting weather...")
 
+	let openweather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${a}&lon=${b}&units=metric&appid=7217d8925634726c87adcf087ea90583`)
+	console.log(JSON.stringify(openweather.data, null, 2))
 
-//Gets the user's current location (lat and Long)
-export const getLocation = () => {
-	if ("geolocation" in navigator) {
-		console.log("Available")
-		navigator.geolocation.getCurrentPosition((position) => {
-			console.log("Latitude is :", position.coords.latitude)
-			console.log("Longitude is :", position.coords.longitude)
-			this.setState({
-				latitude: position.coords.latitude,
-				longitude: position.coords.longitude
-			})
-			console.log(this.props.latitude)
-			return
-		})
-	} else {
-		console.log("Not Available");
+	return {
+		weather: openweather.data.weather[0].main,
+		temp: Math.floor(openweather.data.main.temp),
+		feelsLike: Math.floor(openweather.data.main.feels_like),
+		humidity: openweather.data.main.humidity,
+		windspeed: Math.floor(openweather.data.wind.speed)
 	}
 
+	// this.getRandomPlaylist(openweather.data.weather[0].main)
 }
+
+// getBackgroundVideo = () => {
+// 	if this.state.weather == "clouds" return "https://asset.cloudinary.com/djxvdruvu/41bbc3f896a0d3ceff205ecad4273ab6"
+// }
+
+export {getWeather}
