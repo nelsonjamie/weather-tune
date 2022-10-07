3c21e1b91ee88837e5cb6acfd17c3eb167d1cb5f
Jamie Nelson <108233670+nelsonjamie@users.noreply.github.com>
2022-10-07 15:44:13 +0700

    Update spotifyFunctions.js

--- src/spotifyFunctions.js
+++ src/spotifyFunctions.js
@@ -9,67 +9,42 @@ const RESPONSE_TYPE = 'token'
 const params = new Proxy(new URLSearchParams(window.location.search), {get: (searchParams, prop) => searchParams.get(prop),});
 const token = window.location.hash.substring(1).split("=")[1]
 
+export const checkIsLoggedIn = () => {
+	return token != undefined
+}
 
 export const loginEndpoint = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`
 
 console.log({ loginEndpoint });
 
 
-export const searchPlaylists = async (token, query) => {
+export const searchPlaylists = async (query) => {
+	const ENDPOINT = 'https://api.spotify.com/v1/search?q='+ query+'&type=playlist&include_external=audio&limit=50'
 
+		let data = await axios.get(ENDPOINT, {
+			headers: {
+				Authorization: 'Bearer ' + token ,
+				"Content-Type": "application/json"
 
-const ENDPOINT = 'https://api.spotify.com/v1/search?q='+ query+'&type=playlist&include_external=audio&limit=50'
+			}
+		})
 
-	let data = await axios.get(ENDPOINT, {
-		headers: {
-			Authorization: 'Bearer ' + token ,
-			"Content-Type": "application/json"
-
-		}
-	})
-
-	console.log(data);
- 	return data;
+		console.log(data);
+	 	return data;
 }
 
 // for the Spotify Api
 
 // function that gives me a random playlist from the array that the spotify api returns
-export const getRandomPlaylist = async (a) => {
-	console.log("getting random playlist...");
-
-// Returns the token from the url and using the substring and split it separates the token from the "#access_token"
-
-	let spotifyData = []
-
-	// when user connects and is authorized you can receive a token and search for a playlist
-	if (token) {
-		spotifyData = await searchPlaylists(token, a)
-		this.setState({
-			loggedIn: true
-		})
-	} else {
-		console.log("Invalid token");
-	}
-	console.log("spotify data: ");
-	console.log(spotifyData);
+export const getRandomPlaylist = async (spotifyData) => {
 
 	//setting the playlist items to a variable
 	let resultPlaylists = spotifyData.data.playlists.items;
-	console.log("resultPlaylists");
-	console.log(resultPlaylists);
 
 	// here we are looping through the results/items anc choosing a random playlist
 	let randomPlaylist = resultPlaylists[Math.floor(Math.random() * resultPlaylists.length)].id;
 
-	console.log("the random playlist is...");
 	console.log(randomPlaylist);
-
-	this.setState( {
-		playlistID: randomPlaylist
-	})
 
 	return randomPlaylist
 }
-
-export default { loginEndpoint, searchPlaylists, getRandomPlaylist }
